58dccac70f89d03e3204c08e39090d3d16ce2626
Peter Jin <peterhaijin@gmail.com>
2019-02-11 18:13:49 -0800

    Don't emit rustdoc for dependencies. Use nightly rustdoc.

--- docs/aliases.js
+++ docs/aliases.js
@@ -1,3 +1,2 @@
 var ALIASES = {};
-ALIASES['cuda'] = {};
-ALIASES['static_assertions'] = {};
+ALIASES["cuda"] = {};
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex['cuda'] = {"name":"","dirs":[{"name":"ffi","dirs":[{"name":"v10_0","dirs":[],"files":["_cublas.rs","_cuda.rs","_cuda_fp16.rs","_cuda_runtime_api.rs","_curand.rs","_driver_types.rs","_library_types.rs"]}],"files":["mod.rs"]}],"files":["driver.rs","extras.rs","lib.rs","runtime.rs"]};
-sourcesIndex['static_assertions'] = {"name":"","dirs":[],"files":["assert_cfg.rs","assert_eq_size.rs","assert_fields.rs","assert_impl.rs","assert_obj_safe.rs","const_assert.rs","lib.rs"]};
+sourcesIndex["cuda"] = {"name":"","dirs":[{"name":"ffi","dirs":[{"name":"v10_0","dirs":[],"files":["_cublas.rs","_cuda.rs","_cuda_fp16.rs","_cuda_runtime_api.rs","_curand.rs","_driver_types.rs","_library_types.rs"]}],"files":["mod.rs"]}],"files":["driver.rs","extras.rs","lib.rs","runtime.rs"]};
+createSourceSidebar();
